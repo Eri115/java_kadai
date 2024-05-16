269ef02c1779f422faee1927c379a9f0f3ca251d
eri <k.erii0341115@gmail.com>
2024-05-16 11:31:57 +0900

    validation_html

--- javascripts/sale.js
+++ javascripts/sale.js
@@ -1,7 +1,7 @@
 const productSelect = document.getElementById("product");
 const number = document.getElementById("number"); 
 const products = [
-  {
+  {//これは配列
     id: 1,
     name: "オリジナルブレンド200g",
     price: 500
@@ -27,27 +27,29 @@ let purchases = [];
 
 function add() {
   // 選択した商品のvalueを変数に入れる
+  // parseIntはproductSelectとvalueを比較するために数値に変換する関数
   const value = parseInt(productSelect.value)
   // 変数に入れたものを数字に変換
   const product = products.find((element) => element.id == value);
-
-  // products変数に格納されているデータの中から取得
-  //const products = parseInt("product");
+  // products変数に格納されているデータの中から一つずつとって見つけ出す関数。それを比較演算子で比較する
+  // const products = parseInt("product");
   const selectnumber = parseInt(number.value);
   let purchase = {
     product: product,
     number: selectnumber,
   };
   purchases.push(purchase);
-  window.alert(`${display()}小計\n${subtotal()}`);
+  window.alert(`${display()}小計\n${subtotal()}円`);
 }
-
+//関数
+//関数に対しの引数
+//戻り値は何か
 
 function display() {
   let string = "";
   for (let i = 0; i < purchases.length; i++) {
    
-    string += `${purchases[i].product.price}円が${purchases[i].number}点\n`;
+    string += `${purchases[i].product.name}${purchases[i].product.price}円が${purchases[i].number}点\n`;
   }
   return string;
 }
@@ -64,14 +66,15 @@ function subtotal (){
 
 function calc() {
   const sum = subtotal();
-  const postage = calcPostageFrompurchase(sum);
+  debugger
+  const postage = calcPostageFrompurchase();
   window.alert(`小計は${sum}円、送料は${postage}円です。合計は${sum + postage}円です`);
   purchases = [];
   product = "";
   number = "";
 }
 
-function calcPostageFrompurchases(小計){
+function calcPostageFrompurchases(){
   if (sum == 0 < 3000) {
     return  0;
   } else if (sum < 2000) {
---
+++ javascripts/sample.js
@@ -0,0 +1,3 @@
+const parent = $("parent");
+console.log (parent)
+
